f6be1122d81737279c7ecd644e559e5196a559d5
zyp <2862355856@qq.com>
2023-03-15 22:58:28 +0800

    modified model and fix some bugs

--- infer.py
+++ infer.py
@@ -10,23 +10,25 @@ from model import *
 # model = ChartNet(20, 20, 200, 512)
 # y = model(torch.randn(3, 1, 128, 87))
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = ChartNet(600, 500, 512, 2, 256).to(device)
+model = ChartNet(600, 500, 512, 2, 16).to(device)
 model.eval()
-model.load_state_dict(torch.load('checkpoints/c.pth', map_location=device))
+model.load_state_dict(torch.load('checkpoints/e_9.pth', map_location=device))
 
 
-audio_path = "Song.ogg"
+audio_path = "canon.flac"
 # read the audio and compute the mel spectrogram
 try:
     y, sr = librosa.load(audio_path)
 except BaseException:
     print("audio load fail")
-
+print(sr)
 hop_length = 512
-mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, hop_length=hop_length)
+mel_spectrogram = librosa.feature.melspectrogram(
+    y=y, sr=sr, hop_length=hop_length)
 mel_spectrogram = torch.tensor(mel_spectrogram)
-bpm, beat = librosa.beat.beat_track(y=y, sr=sr, hop_length=hop_length, units="time")
-offset = beat[0] * 1000
+bpm, beat = librosa.beat.beat_track(
+    y=y, sr=sr, hop_length=hop_length, units="time")
+offset = offset = (-beat[0] * 1000) % (60/bpm*1000)
 
 # compute the number of possible beats, every beat is divided into 4 sections
 audio_length = len(y) / sr + offset / 1000
@@ -36,10 +38,11 @@ period = 60 / (bpm * 4)
 
 # convert mel spectrogram into data_temp
 # copied from preprocessing.py
-data_temp = {"input": torch.zeros([beat_num, 1, 128, 87],device=device)}
+data_temp = {"input": torch.zeros([beat_num, 1, 128, 87], device=device)}
 for i in range(beat_num):
-    time_now = offset / 1000 + period * i
-    frame_now = librosa.time_to_frames(times=time_now, sr=sr, hop_length=hop_length)
+    time_now = - offset / 1000 + period * i
+    frame_now = librosa.time_to_frames(
+        times=time_now, sr=sr, hop_length=hop_length)
     if frame_now - 43 < 0:
         data_temp["input"][i][0] = torch.cat(
             (torch.zeros([128, 43 - frame_now]), mel_spectrogram[:, 0: frame_now + 43 + 1]), dim=1)
@@ -48,27 +51,34 @@ for i in range(beat_num):
             (mel_spectrogram[:, frame_now - 43:],
              torch.zeros([128, 87 - mel_spectrogram[:, frame_now - 43:].shape[1]])), dim=1)
     else:
-        data_temp["input"][i][0] = mel_spectrogram[:, frame_now - 43: frame_now + 43 + 1]
+        data_temp["input"][i][0] = mel_spectrogram[:,
+                                                   frame_now - 43: frame_now + 43 + 1]
 
-# torch.set_printoptions(profile="full")
+
 # print(data_temp["input"].shape)
 outputs = model(data_temp["input"])
-outputs = torch.argmax(outputs, dim=1)
-
+outputs=torch.reshape(outputs,[-1,4,4])
+print(outputs.shape)
+# outputs = torch.argmax(outputs, dim=1)
 
 
 # define the chart
 chart = {}
-chart["meta"] = {"version": "4K ADVANCED Lv.19", "mode": 0, "mode_ext": {"column": 4}}
+chart["meta"] = {"version": "AI generated",
+                 "mode": 0, "mode_ext": {"column": 4}}
 chart["time"] = [{"beat": [0, 0, 1], "bpm": bpm}]
 chart["note"] = []
 
 chart_temp = torch.zeros(beat_num, 4)
 for i in range(beat_num):
-    chart_temp[i][0] = int(outputs[i] / 1) % 4
-    chart_temp[i][1] = int(outputs[i] / 4) % 4
-    chart_temp[i][2] = int(outputs[i] / 16) % 4
-    chart_temp[i][3] = int(outputs[i] / 64) % 4
+    # chart_temp[i][0] = int(outputs[i] / 1) % 4
+    # chart_temp[i][1] = int(outputs[i] / 4) % 4
+    # chart_temp[i][2] = int(outputs[i] / 16) % 4
+    # chart_temp[i][3] = int(outputs[i] / 64) % 4
+    chart_temp[i] = torch.argmax(outputs[i], dim=0)
+
+torch.set_printoptions(profile="full")
+print(chart_temp)
 
 
 def beatindex2note(index):
@@ -79,7 +89,8 @@ for i in range(beat_num):
     for col in range(4):
         if chart_temp[i][col] == 1:
             beatindex, section = beatindex2note(i)
-            chart["note"].append({"beat": [beatindex, section, 4], "column": col})
+            chart["note"].append(
+                {"beat": [beatindex, section, 4], "column": col})
         elif chart_temp[i][col] == 2:
             beatindex_s, section_s = beatindex2note(i)
             for j in range(i + 1, beat_num):
@@ -91,8 +102,10 @@ for i in range(beat_num):
                 elif chart_temp[j][col] != 0:
                     break
 
-chart["note"].append({"beat": [0, 0, 1], "sound": audio_path, "vol": 100, "offset": offset, "type": 1})
-chart["extra"] = {"test": {"divide": 4, "speed": 100, "save": 0, "lock": 0, "edit_mode": 0}}
+chart["note"].append({"beat": [0, 0, 1], "sound": audio_path,
+                     "vol": 100, "offset": int(offset), "type": 1})
+chart["extra"] = {"test": {"divide": 4, "speed": 100,
+                           "save": 0, "lock": 0, "edit_mode": 0}}
 
 with open("AIchart.mc", "w", encoding="utf-8") as f:
     json.dump(chart, f)
